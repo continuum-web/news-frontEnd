d56d6ab0f9216b8190c560d656e8e6962496b478
Scott Wears <scottwears1@outlook.com>
2021-11-26 10:52:26 +0000

    adds the functionality to vote only once per refresh

--- src/components/ChangeArticleVotes.jsx
+++ src/components/ChangeArticleVotes.jsx
@@ -2,30 +2,32 @@ import React, { useState } from 'react';
 import { upVoteArticle } from '../utils/ApiCalls';
 
 export default function ChangeArticleVotes({ article_id, votes }) {
-	
-
 	const [voteModifier, setVoteModifier] = useState(0);
-	
-    const upVote = article_id => {
-       
+	const [voted, setVoted] = useState(false)
+	const [buttonText, setButtonText] = useState("Vote")
+	const upVote = article_id => {
 		setVoteModifier(votesModifier => {
-			return votesModifier ;
+			return votesModifier;
 		});
 		upVoteArticle(article_id);
 	};
 
 	return (
 		<div>
-			<span className='singlearticleVotes'>Votes: {votes + voteModifier }</span>
+			<span className='singlearticleVotes'>Votes: {votes + voteModifier}</span>
 			<button
 				className='singlearticleButton'
 				onClick={() => {
-					setVoteModifier(voteModifier => {
-						return voteModifier + 1;
-					});
-					upVote(article_id);
+					if (!voted) {
+						setVoteModifier(voteModifier => {
+							return voteModifier + 1;
+						});
+						upVote(article_id);
+						setVoted(true)
+						setButtonText('voted')
+					} 					
 				}}>
-				Add Vote
+				{ buttonText}
 			</button>
 		</div>
 	);
